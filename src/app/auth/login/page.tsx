9dacd9ce3119fdff94d4ade1e32e89916638ce39
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-03-17 00:03:56 +0530

    feat: 🦄 add zod and form validation for auth

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,23 +1,54 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 export default function Login() {
+  const formSchema = z.object({
+    userName: z.string().min(1, "Username is required "),
+    password: z.string().min(1, "Password is required "),
+  });
+  type formType = z.infer<typeof formSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onsubmit = () => {
+    reset();
+  };
+
   return (
     <main className="min-h-screen bg-lightBeige flex flex-col items-center justify-center w-full">
       <h1 className="text-5xl font-black font-mono mb-10">LOGIN</h1>
 
-      <form className="flex flex-col space-y-3 items-center">
+      <form
+        onSubmit={handleSubmit(onsubmit)}
+        className="flex flex-col space-y-3 items-center"
+      >
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.userName && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+          {...register("userName")}
           placeholder="Username"
           type="text"
         />
 
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.password && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+          {...register("password")}
           placeholder="Password"
           type="text"
         />
 
         <button
           type="submit"
+          disabled={isSubmitting}
           className="font-mono font-black p-5 max-w-fit h-10 inline-flex justify-center border border-accBlack rounded-md items-center bg-satRed text-lightBeige hover:bg-satRed-hover hover:text-white"
         >
           Login
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,35 +1,69 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 export default function SignUp() {
+  const formSchema = z.object({
+    name: z.string().min(1, "Name is required "),
+    username: z.string().min(1, "Name is required "),
+    password: z.string().min(1, "Name is required "),
+    location: z.string().min(1, "Name is required "),
+  });
+  type formType = z.infer<typeof formSchema>;
+
+  const onsubmit = () => {
+    reset();
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+  });
+
   return (
     <main className="min-h-screen bg-lightBeige flex flex-col items-center justify-center w-full">
       <h1 className="text-5xl font-black font-mono mb-10"> SIGN UP</h1>
 
-      <form className="flex flex-col space-y-3 items-center">
+      <form
+        onSubmit={handleSubmit(onsubmit)}
+        className="flex flex-col space-y-3 items-center"
+      >
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
           placeholder="Name"
+          {...register("name")}
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.name && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
           type="text"
         />
 
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
           placeholder="Username"
+          {...register("username")}
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.username && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
           type="text"
         />
 
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
           placeholder="Location"
+          {...register("location")}
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.location && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
           type="text"
         />
 
         <input
-          className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
           placeholder="Password"
+          {...register("password")}
+          className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.password && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
           type="text"
         />
 
         <button
           type="submit"
+          disabled={isSubmitting}
           className="font-mono font-black p-5 max-w-fit h-10 inline-flex justify-center border border-accBlack rounded-md items-center bg-satRed text-lightBeige hover:bg-satRed-hover hover:text-white"
         >
           Sign Up
