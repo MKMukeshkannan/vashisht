7a62f46514028bd63e24cb12368a03e88dc01959
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2024-03-16 07:14:19 +0530

    feat: 🐻 api merged

--- src/components/ProductDescription.tsx
+++ src/components/ProductDescription.tsx
@@ -5,6 +5,7 @@ interface prop {
   retailer: string;
   price: number;
   category: string;
+  image_url: string;
 }
 
 export default function ProductDescription({
@@ -12,14 +13,16 @@ export default function ProductDescription({
   price,
   category,
   retailer,
+  image_url
 }: prop) {
+
   return (
     <section className="flex flex-col w-full min-w-64 p-3 lg:max-w-64 bg-red-100 border-4 border-black">
       <section className="relative w-full h-0 flex-grow">
         <Image
           fill
           className="object-cover object-center border-black border-2"
-          src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcR2tefi5ek26GZJH9dNXLA7V6LYLOcKuJNmm5V_22X_pibyy2dF_gr1goKFtv--pe50FLi7fMNCASyHhXhRweb6QWTy6sTwg2GPu0QDaa_shLIHyFbX5yxCZw"
+          src={image_url}
           alt="java"
         />
       </section>
