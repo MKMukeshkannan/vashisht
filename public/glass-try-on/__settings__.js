6847b0f2646fc1fb42dcda962ae16a308d581d31
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-03-16 19:34:06 +0530

    feat: 🐑 add AR Feature

---
+++ public/glass-try-on/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1968965.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'preferWebGpu': false,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 171505463, 171505421, 171505420, 171505454, 171505449, 171505450, 171505458 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
---
+++ src/app/try-out/page.tsx
@@ -0,0 +1,7 @@
+export default function TryOut() {
+  return (
+    <main className="min-h-screen bg-lightBeige w-full">
+      <iframe className="w-full h-screen" src="./glass-try-on/index.html" />
+    </main>
+  );
+}
