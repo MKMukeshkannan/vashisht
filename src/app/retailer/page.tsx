fb7c4561302282a24129b880832eb4f094d6b05b
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-03-16 23:48:13 +0530

    feat: 🐘 restructure retailer form route

--- src/app/retailer/page.tsx
+++ src/app/retailer/page.tsx
@@ -5,17 +5,46 @@ import { API_URL } from "@/libs/config";
 import axios from "axios";
 import { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
+import { useForm } from "react-hook-form";
 import { toast, ToastContainer, ToastOptions } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
 export default function Admin() {
-  const [name, setName] = useState<string>("");
-  const [retailer, setRetailer] = useState<string>("");
-  const [price, setPrice] = useState<string>("");
-  const [brand, setBrand] = useState<string>("");
   const [productimage, setProductImage] = useState<File | undefined>(undefined);
-  const [texture, setTexture] = useState<File | undefined>(undefined);
-  const [category, setCategory] = useState<string>("");
+  const formSchema = z.object({
+    name: z
+      .string({ required_error: "Name feild is required" })
+      .min(4, "Name should be atleast 4 characters")
+      .max(15, "Name should be atmost 15 characters"),
+    price: z
+      .string({ required_error: "Price feild is required" })
+      .min(1, "Price feild is required"),
+    color: z
+      .string({ required_error: "Color feild is required" })
+      .min(1, "Price feild is required"),
+    category: z
+      .string({ required_error: "Category feild is required" })
+      .min(1, "Price feild is required"),
+    size: z
+      .string({ required_error: "Size feild is required" })
+      .min(1, "Price feild is required"),
+    description: z
+      .string({ required_error: "Description feild is required" })
+      .min(1, "Price feild is required"),
+  });
+  type formType = z.infer<typeof formSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<formType>({
+    resolver: zodResolver(formSchema),
+  });
 
   const toastOptions: ToastOptions = {
     position: "top-right",
@@ -28,119 +57,73 @@ export default function Admin() {
     theme: "light",
   };
 
-  const handlesubmit = (event: any) => {
-    event.preventDefault();
+  const onSubmit = (data: formType) => {
+    const formData = new FormData();
 
-    // HANDLE ERROR
-    if (name.length === 0) {
-      return toast.warn("Name is required", toastOptions);
-    }
-    if (retailer.length === 0) {
-      return toast.warn("Retailer is required", toastOptions);
-    }
+    if (!productimage) return toast.error("Upload File");
 
-    if (!price) {
-      return toast.warn("Price is required", toastOptions);
-    }
-    if (isNaN(parseInt(price))) {
-      if (parseInt(price) <= 0) {
-        return toast.warn("Price should be positive integer", toastOptions);
-      }
-      return toast.warn("Price should be number", toastOptions);
-    }
-    if (brand.length === 0) {
-      return toast.warn("Brand is required", toastOptions);
-    }
-    if (category.length === 0) {
-      return toast.warn("Category is required", toastOptions);
-    }
-    if (!["top", "bottom", "foot"].includes(category)) {
-      return toast.warn('Category can only be "top" "botton" or "foot" ');
-    }
-    if (texture === undefined) {
-      return toast.warn("Upload a texture", toastOptions);
-    }
-    if (productimage === undefined) {
-      return toast.warn("Upload Product image", toastOptions);
-    }
-    toast.info("Uploading!! Hold tight 🙏🏼");
-    if (name && retailer && price && brand && productimage && texture) {
-      const formData = new FormData();
+    formData.append("image", productimage);
+    formData.append("name", data.name);
+    formData.append("price", data.price);
+    formData.append("color", data.color);
+    formData.append("category", data.category);
+    formData.append("size", data.size);
+    formData.append("description", data.description);
 
-      formData.append("image", productimage);
-      formData.append("name", name);
-      formData.append("brand", brand);
-      formData.append("retailer_name", retailer);
-      formData.append("price", price);
-      formData.append("texture", texture);
-      formData.append("category", category);
-
-      axios
-        .post(`${API_URL}api/admin/upload-product`, formData, {
-          headers: { "Content-Type": "multipart/form-data" },
-        })
-        .then(() => {
-          setPrice("");
-          setProductImage(undefined);
-          setTexture(undefined);
-          setName("");
-          setBrand("");
-          setCategory("");
-          setRetailer("");
-          toast("SUCESS 🎉", toastOptions);
-        })
-        .catch(() => {
-          toast.error("ERROR ❌", toastOptions);
-        });
-    }
+    axios
+      .post(`${API_URL}api/admin/upload-product`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then(() => {
+        reset();
+        toast("SUCESS 🎉", toastOptions);
+      })
+      .catch(() => {
+        toast.error("ERROR ❌", toastOptions);
+      });
   };
 
   return (
     <main className="flex min-h-screen items-right bg-lightBeige p-4 lg:p-24">
       <section className="w-fit">
         <h1 className="text-5xl font-black font-mono mb-10">ADMIN</h1>
+
         <form
           className="flex flex-col max-w-96 space-y-5"
-          onSubmit={handlesubmit}
+          onSubmit={handleSubmit(onSubmit)}
         >
           <input
-            className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.name && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
             placeholder="Name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
-            placeholder="Retailer"
-            type="text"
-            value={retailer}
-            onChange={(e) => setRetailer(e.target.value)}
-          />
-          <input
-            className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
-            placeholder="Price"
-            type="text"
-            value={price}
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          />
-          <input
-            className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
-            placeholder="Brand"
-            type="text"
-            value={brand}
-            onChange={(e) => setBrand(e.target.value)}
-          />
-          <input
-            className="shadow-[5px_5px_0px_0px_rgba(0,0,0)] h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono"
-            placeholder="Category"
-            type="text"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            {...register("name")}
           />
 
+          <input
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.price && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+            placeholder="Price"
+            {...register("price")}
+          />
+          <input
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.color && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+            placeholder="Color"
+            {...register("color")}
+          />
+          <input
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.category && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+            placeholder="Category"
+            {...register("category")}
+          />
+
+          <input
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.size && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+            placeholder="Size"
+            {...register("size")}
+          />
+          <input
+            className={`shadow-[5px_5px_0px_0px_rgba(0,0,0)] ${errors.description && "shadow-[5px_5px_0px_0px_rgba(220,0,0)]"} h-10 text-2xl rounded-md border border-accBlack focus:outline-none p-1.5 font-mono`}
+            placeholder="Description"
+            {...register("description")}
+          />
           <section>
             <h1 className="inline-flex text-xl font-black font-mono">
               Product Image
@@ -152,17 +135,8 @@ export default function Admin() {
             />
           </section>
 
-          <section>
-            <h1 className="inline-flex w-2/3 text-xl font-black font-mono">
-              Texture Map
-            </h1>
-            <FileUploader
-              handleChange={(file: File) => setTexture(file)}
-              name="file"
-              types={["JPG", "PNG", "JPEG"]}
-            />
-          </section>
           <button
+            disabled={isSubmitting}
             type="submit"
             className="font-mono font-black p-5 max-w-fit h-10 inline-flex justify-center border border-accBlack rounded-md items-center bg-satRed text-lightBeige hover:bg-satRed-hover hover:text-white"
           >
@@ -184,7 +158,7 @@ export default function Admin() {
       </section>
 
       <section className="w-full flex flex-col px-16">
-        <h1  className="text-5xl font-black font-mono mb-10">Your Products</h1>
+        <h1 className="text-5xl font-black font-mono mb-10">Your Products</h1>
         <section className="grid grid-cols-3 h-0 flex-grow overflow-auto ">
           <ProductDescription
             image_url="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png"
