980669858859605f081980ee8d2dae0d2b9a8189
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-03-17 19:34:40 +0530

    fix: 🐩 build fixes (loadin from env)

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://helical-indexer-408416.uc.r.appspot.com/";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 export const OPEN_AI_KEY = process.env.NEXT_PUBLIC_OPEN_AI_KEY || "";
